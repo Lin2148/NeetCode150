8ebdb51d7af37c83005d8b2c6850f0e4241a7b53
Lin2148 <qq210487@gmail.com>
2026-03-24 21:47:27 +0800

    Time: 0 ms (100%), Space: 44.4 MB (31.46%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,20 +10,12 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null && list2 ==null) return list1;  //全空
-        else if (list1==null) return list2;
-        else if (list2==null ) return list1;
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
         
         //都有值
-        ListNode head,curr;
-        if (list1.val <= list2.val){
-            head = list1;
-            list1 = list1.next;
-        } else{
-            head = list2;
-            list2 = list2.next;
-        }
-        curr = head;
+        ListNode sentinel = new ListNode();
+        ListNode curr = sentinel;
 
         while ((list1 != null) && (list2 != null)){
             if (list1.val <= list2.val){
@@ -37,11 +29,8 @@ class Solution {
         }
 
         //有一條走完了 沒有val
-        if (list1 != null) {
-            curr.next = list1;
-        } else if (list2 != null) {
-            curr.next = list2;
-        }
-        return head;
+        curr.next = (list1 != null) ? list1 : list2;
+
+        return sentinel.next;
     }
 }
