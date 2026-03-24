4e3df5e94e6cc102f59e8797b73946c7da6da38a
Lin2148 <qq210487@gmail.com>
2026-03-24 18:04:50 +0800

    Time: 5 ms (60.39%), Space: 44.6 MB (58.25%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()){
+             return false;
+        }
+
+        int[] cnt = new int[26];
+
+        for(char c : s.toCharArray()){
+            cnt[c-'a'] += 1;
+        }
+        for(char c : t.toCharArray()){
+            cnt[c-'a'] -= 1;
+            if (cnt[c-'a'] < 0){
+                return false;
+            }
+        }       
+
+        for(int i : cnt){
+            if (i != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
