9c355783e64f21a82dbd8ffa9fab3cd7d752354a
Lin2148 <qq210487@gmail.com>
2026-03-24 18:27:28 +0800

    Time: 2 ms (99.27%), Space: 44.2 MB (81.87%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,20 +1,22 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String nospaceLowerS = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        if (s.isEmpty()) return true;
+
         int first = 0;
-        int last = nospaceLowerS.length() - 1;
-
-        while(first <= last){
-            char sFirst = nospaceLowerS.charAt(first);
-            char sLast = nospaceLowerS.charAt(last);
-
-            if (sFirst == sLast){
+        int last = s.length()-1; 
+        while (first <last){
+            char ptrfirst = s.charAt(first);
+            char ptrlast = s.charAt(last);
+            if (Character.isLetterOrDigit(ptrfirst)==false) first++;
+            else if (Character.isLetterOrDigit(ptrlast)==false) last--;
+            else {
+                if (Character.toLowerCase(ptrfirst)!=Character.toLowerCase(ptrlast)){
+                    return false;
+                }
                 first++;
                 last--;
-            } else{
-                return false;
             }
-        }
+        } 
         return true;
     }
 }
