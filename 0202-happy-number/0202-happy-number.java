c66d7f2bfb3d395dd93ed76d52ad495e2b9cd75c
Lin2148 <qq210487@gmail.com>
2026-03-28 01:12:04 +0800

    Time: 1 ms (78.05%), Space: 42.1 MB (92.89%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,5 +1,34 @@
 class Solution {
     public boolean isHappy(int n) {
+        // 2 ptr method less space
+        // fast cal. 1 time first to get into loop
+        int slowPtr = n;
+        int fastPtr = cal(n);
+        
+        while (fastPtr != 1 && slowPtr != fastPtr){
+            slowPtr = cal(slowPtr);
+            fastPtr = cal(cal(fastPtr));
+        }
+        
+        return (fastPtr == 1);
+    }
+
+    // cal ss
+    private int cal(int n){
+
+        int sum = 0;
+        while (n > 0){
+            int quotient = n % 10;
+            sum += (quotient % 10) * (quotient % 10); 
+            n /= 10;
+        }
+        return sum;
+    }
+}
+
+/*
+class Solution {
+    public boolean isHappy(int n) {
         // save every time sum of square ans. to check loop
         HashSet<Integer> set = new HashSet<>();
         
@@ -21,3 +50,4 @@ class Solution {
     return false;
     }
 }
+*/
