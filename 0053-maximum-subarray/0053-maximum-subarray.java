fb805cf7fd16f0825c9e4c9e7212c770c238e218
Lin2148 <qq210487@gmail.com>
2026-04-10 18:28:07 +0800

    Time: 1 ms (99.98%), Space: 77.2 MB (55.47%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,20 +1,12 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         int max = nums[0];
+        int curMax = nums[0];
         int length = nums.length;
-        int[] curMax = new int[length];
-        curMax[0] = nums[0];
+
         for (int i = 1; i < length; i++){
-            if (curMax[i-1] + nums[i] >= nums[i]){
-                curMax[i] = curMax[i-1] + nums[i]; 
-                if (curMax[i] > max){
-                    max = curMax[i];
-                }
-            }
-            else {
-                curMax[i] = nums[i];
-                max = Math.max(max, curMax[i]);
-            }
+            curMax = Math.max(nums[i], curMax + nums[i]);
+            max = Math.max(max, curMax);
         }
         return max;
     }
