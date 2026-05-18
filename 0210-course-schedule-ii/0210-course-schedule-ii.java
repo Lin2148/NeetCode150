618a2eebdeeb56834684f0f30b732667fb4144d5
Lin2148 <qq210487@gmail.com>
2026-05-18 22:34:20 +0800

    Time: 3 ms (99.48%), Space: 46.5 MB (90.3%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+         //全部先修的關係結構
+        List<Integer>[] adj = new ArrayList[numCourses];
+
+        for (int i = 0; i < numCourses; i++){
+            adj[i] = new ArrayList<>();
+        }
+        for (int[] pre: prerequisites){
+            adj[pre[1]].add(pre[0]);
+        }
+        // 紀錄結果 如果可修完課
+        Deque<Integer> result = new ArrayDeque<>();
+
+        // 紀錄狀態 0未拜訪 1當前路徑 2拜訪完成
+        int[] visit = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!dfs(i, visit, adj, result)) {
+                return new int[0];
+            }
+        }
+        // 確認有order
+        int[] ans = new int[numCourses];
+        int i = 0;
+        for (int course : result) {
+            ans[i] = course;
+            i++;
+        }
+        return ans;
+    }
+
+    private boolean dfs(int course, int[] visit, List<Integer>[] adj, Deque<Integer> result){
+        if (visit[course] == 1){
+            return false;
+        }
+        if (visit[course] == 2){
+            return true;
+        }
+
+        visit[course] = 1;
+        // list裡面每個點都DFS
+        for (int i : adj[course]){
+            if (!dfs(i, visit, adj, result)) {
+                return false;
+            }
+        }
+
+        // 該點結束
+        visit[course] = 2;
+        result.addFirst(course);
+        return true;
+    }
+}
