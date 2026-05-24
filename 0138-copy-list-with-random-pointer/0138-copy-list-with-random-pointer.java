26cb033970c0ea328a61e808d68ee58aa63d0900
Lin2148 <qq210487@gmail.com>
2026-05-24 15:52:25 +0800

    Time: 0 ms (100%), Space: 46.5 MB (49.98%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -12,7 +12,47 @@ class Node {
     }
 }
 */
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
 
+        //  把list轉為 old -> copy -> old-> copy形式
+        Node curr = head;
+        while (curr != null){
+            Node copy = new Node(curr.val);
+            copy.next = curr.next;
+            curr.next = copy;
+
+            curr = curr.next.next;
+        }
+
+        // 調整random
+        Node curr2 = head;
+        while (curr2 != null){
+            if (curr2.random != null){
+                curr2.next.random = curr2.random.next;
+            }
+            curr2 = curr2.next.next;
+        }
+
+        // 拆list
+        Node old = head;
+        Node copy = head.next;
+        Node curr3 = copy;
+        while (old != null){
+            old.next = old.next.next;
+            if (curr3.next != null) {
+                curr3.next = curr3.next.next;
+            } else {
+                curr3.next = null; // 到達新串列的終點了
+            }
+            old = old.next;
+            curr3 = curr3.next;
+        }
+        return copy;
+    }
+}
+/*
 class Solution {
     public Node copyRandomList(Node head) {
         Map<Node, Node> map = new HashMap<>();
@@ -36,3 +76,4 @@ class Solution {
         return map.get(head);
     }
 }
+*/
