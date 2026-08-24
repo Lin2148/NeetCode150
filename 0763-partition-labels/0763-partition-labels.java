5c4daf21526623932cfd3838db58fa1508e8abf0
YUNG LING CHEN <qq210487@gmail.com>
2026-08-24 16:40:54 +0800

    Time: 10 ms (23.48%), Space: 44.3 MB (7.11%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> ans = new ArrayList<>();
+        //store each char last appear position
+        Map<Character,Integer> hm = new HashMap<>();
+        for (int i = 0; i < s.length();i++){
+            hm.put(s.charAt(i),i);
+        }
+
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < s.length(); i++){
+            end = Math.max(end,hm.get(s.charAt(i)));
+            if (i==end){
+                ans.add(i-start+1);
+                start=i+1;
+            }
+        }
+        return ans;
+    }
+}
