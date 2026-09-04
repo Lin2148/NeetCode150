4211e8e7fc885cc27820ca2ecf71ab769844f8fc
YUNG LING CHEN <qq210487@gmail.com>
2026-09-05 00:29:38 +0800

    Time: 4 ms (99.56%), Space: 47.7 MB (10.59%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int ans = 0;
+        for (int i = 0; i < grid.length; i++){
+            for (int j=0; j < grid[0].length; j++){
+                if (grid[i][j] == 1){
+                    ans += 4;
+                    if (i+1 < grid.length){
+                        if (grid[i+1][j] == 1){
+                            ans -= 2;
+                        }
+                    }
+                    if (j+1 < grid[0].length){
+                        if (grid[i][j+1] == 1){
+                            ans -= 2;
+                        }
+                    }
+                }
+                
+            }
+        }
+        return ans;
+    }
+}
