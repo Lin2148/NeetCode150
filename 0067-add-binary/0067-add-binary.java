14e67f6451e5ea2be804a88786af2def2dcb5545
YUNG LING CHEN <qq210487@gmail.com>
2026-09-24 19:13:30 +0800

    Time: 1 ms (99.98%), Space: 43.5 MB (53.15%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,22 +1,20 @@
-//tuple 可以用ascii相減得到數字  eg '1'-'0'=1， '0'-'0'=0
-
 class Solution {
     public String addBinary(String a, String b) {
+        int carry = 0;
         StringBuilder sb = new StringBuilder();
         int i = a.length()-1;
         int j = b.length()-1;
-        int carry=0;
-        while(i>=0 || j>=0 || carry==1){
+
+        while(i >= 0 || j >= 0 || carry == 1){
             int da = (i>=0 && a.charAt(i)=='1') ? 1:0;
             int db = (j>=0 && b.charAt(j)=='1') ? 1:0;
 
-            int sum=da+db+carry;
-            int digit=sum%2;
-            carry=sum/2;
+            int digit = da^db^carry;
+            carry = (da+db+carry)/2;
             sb.append(digit);
             i--;
             j--;
         }
-    return sb.reverse().toString();
+        return sb.reverse().toString();
     }
 }
